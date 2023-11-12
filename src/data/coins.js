5bfb86aee13d4610af08fecd95129c7a7b5ba96f
Luis Alonzo <doclaag@gmail.com>
2023-11-12 16:57:08 -0600

    created data/coins.js

---
+++ src/data/coins.js
@@ -0,0 +1,6 @@
+export const coins = [
+    { id: 'USD', name: 'Dolar de Estados Unidos' },
+    { id: 'MXN', name: 'Peso Mexicano' },
+    { id: 'EUR', name: 'Euros' },
+    { id: 'GBP', name: 'Libras' },
+];
